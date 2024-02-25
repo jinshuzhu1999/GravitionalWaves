136fb699698354c5a6be1f337ec96b20a9d62a99
YueLily <58393160+Aiyuelily@users.noreply.github.com>
2024-02-25 17:14:29 +0800

    Delete liucheng directory

--- liucheng/lab3/customrand.m
+++
@@ -1,5 +0,0 @@
-%%
-
-function randabVec = customrand(a,b,N)
-initRandVec = rand(1,N);
-randabVec = initRandVec.*(b-a)+a ;
--- liucheng/lab3/customrandn.m
+++
@@ -1,3 +0,0 @@
-function randnVec = customrandn(a, b, N)
-initRandnVec = randn(1,N);
-randnVec = initRandnVec*b+a ;
